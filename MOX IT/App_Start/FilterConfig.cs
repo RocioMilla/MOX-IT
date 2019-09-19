6d404b5027349dab6ade77ae21d8064f01967e52
Rocio Aylen Milla <rocioaylenamilla@gmail.com>
2019-09-19 14:18:10 -0300

    commit inicial

---
+++ MOX IT/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MOX_IT
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# ABM vuelos
+
+_Pequeña aplicación para MOX IT._ 
+
+## Construido con 🛠️
+
+* [ASP.NET](https://dotnet.microsoft.com/apps/aspnet) - Entorno para aplicaciones web.
